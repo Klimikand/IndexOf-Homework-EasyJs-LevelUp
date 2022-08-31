07169c58e69d23d0a6d3f8a0c8baefba7c7882de
klimikand <klimikand@gmail.com>
2022-08-31 19:33:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// 6. //indexOf - Implement the indexOf function for arrays.
+
+    function indexOf(array, elem) {
+        let result = array.indexOf(elem);
+        return result;
+    }
+
+    let result = indexOf([1, 2, 3], 1)
+    console.log(result)
+    
+
+
+// Проверка
+// indexOf([1, 2, 3], 1)               // 0
+// indexOf([1, 2, 3], 4)               // -1
